6b952b4e28627ab064c5772abb8f46791ef8470b
Дарья Коннова <dashustic@yandex.ru>
2024-01-07 13:35:29 +0300

    db config

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api.db import sessionmanager
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    if sessionmanager.engine is not None:
+        await sessionmanager.close()
 
+
+app = FastAPI(lifespan=lifespan)
