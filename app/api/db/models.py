ee1335eddc0cb78b7d033baeb6bcbdb893bb5986
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2024-01-07 15:20:02 +0300

    Ñcreated_models

--- app/api/db/models.py
+++ app/api/db/models.py
@@ -1,5 +1,24 @@
+from datetime import datetime
+from enum import Enum as PyEnum
+
+from sqlalchemy import Enum, ForeignKey, UniqueConstraint, func, String, Text
 from sqlalchemy.orm import (DeclarativeBase, Mapped, declared_attr,
-                            mapped_column)
+                            mapped_column, relationship)
+
+
+class UserRole(PyEnum):
+    ADMIN = "admin"
+    USER = "user"
+    MANAGER = "manager"
+
+
+class TaskStatus(PyEnum):
+    CREATED = "created"
+    AT_WORK = "at work"
+    ON_CHECK = "on check"
+    FROZEN = "frozen"
+    CANCEL = "cancel"
+    FINISHED = "finished"
 
 
 class Base(DeclarativeBase):
@@ -10,3 +29,54 @@ class Base(DeclarativeBase):
         return f"{cls.__name__.lower()}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class UserTasksAssociation(Base):
+    __table_args__ = (
+        UniqueConstraint(
+            "user_id",
+            "task_id",
+            name="unique_user_task",
+        ),
+    )
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id"))
+
+    user: Mapped['User'] = relationship(back_populates="user_detail")
+    task: Mapped['Task'] = relationship(back_populates="task_detail")
+
+
+class User(Base):
+
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now,
+    )
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole),
+        default=UserRole.USER,
+        server_default="user"
+    )
+    user_detail: Mapped[list["UserTasksAssociation"]] = relationship(back_populates="user")
+
+
+class Task(Base):
+    name: Mapped[str] = mapped_column(String(155))
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now,
+    )
+    urgency: Mapped[bool] = mapped_column(default=False, server_default='false')
+    status: Mapped[TaskStatus] = mapped_column(
+        Enum(TaskStatus),
+        default=TaskStatus.CREATED,
+        server_default="created"
+    )
+
+    task_detail: Mapped[list["UserTasksAssociation"]] = relationship(back_populates="task")
+
+
