159146fd5391c6bf69fc6f3b88531756cb2f92e2
Дарья Коннова <dashustic@yandex.ru>
2024-01-13 15:01:02 +0300

    create task-schemas

---
+++ app/api/schemas/task.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+from app.api.db import TaskStatus
+
+
+class TaskSchema(BaseModel):
+    id: int
+    name: str = Field(max_length=155)
+    description: str | None = None
+    created_at: datetime
+    urgency: bool = False
+    status: TaskStatus = TaskStatus.CREATED
+    task_detail: list
+
+
+class GetUserTaskSchema(BaseModel):
+    user_id: int
+    tasks: List[TaskSchema]
