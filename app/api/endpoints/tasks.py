5eb83bdc93d0f1f17f0a56d2a7508375138cf835
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2024-01-13 17:37:15 +0300

    save_local

--- app/api/endpoints/tasks.py
+++ app/api/endpoints/tasks.py
@@ -5,9 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from app.api.db import UserRole, get_db_session, Task
-from app.api.db.models import UserTasksAssociation
-from app.api.endpoints.dependencies import check_role
+from app.api.db.models import UserTasksAssociation, User
+from app.api.endpoints.dependencies import check_role, get_current_user
 from app.api.schemas import TaskSchema
+from app.api.schemas.task import CreateTaskSchema
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
@@ -15,8 +16,11 @@ router = APIRouter(prefix="/tasks", tags=["Tasks"])
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model=TaskSchema)
 @check_role(UserRole.MANAGER)
 async def create_task(
-    task_data: TaskSchema, session: AsyncSession = Depends(get_db_session)
+        task_data: CreateTaskSchema,
+        session: AsyncSession = Depends(get_db_session),
+        user: User = Depends(get_current_user)
 ):
+    print(user.username)
     new_task = Task(
         name=task_data.name,
         description=task_data.description,
--- app/api/schemas/task.py
+++ app/api/schemas/task.py
@@ -15,6 +15,13 @@ class TaskSchema(BaseModel):
     task_detail: list
 
 
+class CreateTaskSchema(BaseModel):
+    name: str = Field(max_length=155)
+    description: str | None = None
+    created_at: datetime
+    urgency: bool = False
+    status: TaskStatus = TaskStatus.CREATED
+
 class GetUserTaskSchema(BaseModel):
     user_id: int
     tasks: List[TaskSchema]
