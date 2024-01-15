700f69b0f398b05ca899e7160c6fd8d66d825c65
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2024-01-15 17:07:30 +0300

    echo=False

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -3,6 +3,7 @@ from passlib.context import CryptContext
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+
 from app.api.db import User, UserRole, get_db_session
 from app.api.endpoints.dependencies import check_role, get_current_user
 from app.api.schemas import CreateUserSchema
@@ -54,3 +55,4 @@ async def get_users(session: AsyncSession = Depends(get_db_session)):
 @check_role(UserRole.ADMIN, UserRole.MANAGER, UserRole.USER)
 async def get_me(user: User = Depends(get_current_user)):
     return user.username
+
