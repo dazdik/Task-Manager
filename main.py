5ea019869f435414ce048eb88de6b4170f61022a
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2024-02-04 21:33:02 +0300

    limit_for_endopoints

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ import os
 from contextlib import asynccontextmanager
 
 import uvicorn
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from fastapi_limiter import FastAPILimiter
 from fastapi_pagination import add_pagination
+from redis import asyncio as aioredis
 from sqladmin import Admin
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api import router
 from app.api.db import sessionmanager
-from app.api.db.admin import (
-    AdminAuth,
-    TaskModelView,
-    UserModelView,
-    UserTasksAssociationModelView,
-)
+from app.api.db.admin import (AdminAuth, TaskModelView, UserModelView,
+                              UserTasksAssociationModelView)
 from app.api.db.admin import router as admin_router
 from app.api.db.settings_db import settings
-from redis import asyncio as aioredis
-
 
 LOCAL_REDIS_URL = "redis://127.0.0.1:6379"
 
@@ -31,6 +26,7 @@ async def lifespan(app: FastAPI):
         "redis://localhost:6379", encoding="utf8", decode_responses=True
     )
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    await FastAPILimiter.init(redis)
 
     yield
     if sessionmanager.engine is not None:
